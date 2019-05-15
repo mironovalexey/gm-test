6fd9abdf17ed517813f8709a60fab37dbe64400b
Alexey Mironov <mironovalexey@yandex.ru>
2019-05-15 11:02:57 +0300

    Hattrs run.

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "github.com/mironovalexey/gm-test/html"
+import "github.com/mironovalexey/gm-test/hattrs"
 
 func main() {
 
 	//note.Run()
-	html.Run()
+	//html.Run()
 	//eof.Run()
+	hattrs.Run()
 
 }
